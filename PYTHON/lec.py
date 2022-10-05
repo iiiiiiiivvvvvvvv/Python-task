906aab1cb9c6cbbd6e69105218d3148f61ca09b1
Igor <109410390+iiiiiiiivvvvvvvv@users.noreply.github.com>
2022-10-05 11:41:17 +0500

    Add files via upload

--- PYTHON/lec.py
+++ PYTHON/lec.py
@@ -142,6 +142,109 @@
     # Задайте список из n чисел последовательности (1+ (1/n))^n
     #  и выведите на экран их сумму.
 
-n = int(input("Введите число "))
-lst = [round((1+1/i)**i, 3) for i in range(1, n+1)]
-print(f'Последовательность: {lst}\nСумма: {round(sum(lst), 3)}')
+# n = int(input("Введите число "))
+# lst = [round((1+1/i)**i, 3) for i in range(1, n+1)]
+# print(f'Последовательность: {lst}\nСумма: {round(sum(lst), 3)}')
+
+# Найти позицию второго вхождения строки в списке или сообщить что её нет
+# text_1 = ["qwe", "asd", "zxc", "ertqwe"]
+# key_word = "qwe"
+# def find_coincidence(new_text, key):
+#     count = 0   
+#     for i in range(len(new_text)):
+#         if new_text[i] == key:
+#             count += 1
+#             if count ==2:
+#                 print(i)
+#                 break
+            
+#         if count < 2:
+#             print("-1")
+# find_coincidence(text_1, key_word)  
+
+
+
+     
+
+
+
+     #Задайте список из нескольких чисел. Напишите программу,
+     # которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+
+
+# a = [2, 3, 6, 10, 12, 16, 5]
+# summ = 0
+# for i in range(1, len(a), 2):
+#     #if i % 2 == 1:
+#         summ += a[i]
+# print(f"{a} сумма элементов на нечётных позициях: {summ}")   
+
+     # Напишите программу, которая найдёт произведение пар чисел списка. 
+     # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+# from random import randint
+
+
+# number = int(input('Введите размер списка '))
+# list = []
+# list2 = []
+
+# for i in range(number):
+#     list.append(randint(0, 9))
+
+# for i in range(len(list)):
+#     while i < len(list)/2 and number > len(list)/2:
+#         number = number - 1
+#         a = list[i] * list[number]
+#         list2.append(a)
+#         i += 1
+
+# print(list)
+# print(list2)
+
+
+    # Задайте список из вещественных чисел. Напишите программу,
+    #  которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+
+# my_list = [1.1, 1.2, 3.1, 5, 10.01]
+# min = 1
+# max = 0
+# for i in my_list:
+#     if (i - int(i)) <= min:
+#         min = i - int(i)
+#     if (i - int(i)) >= max:
+#         max = i - int(i)  
+# print(max-min)
+
+    # Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+# s = ""
+# n = int(input("Введите число для преобразовывания десятичного числа в двоичное:\n"))
+# while n != 0:
+#     s = str(n%2) + s
+#     n //=2
+# print(s)
+
+    # Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+
+# def Fibonacci(n):
+#     if n in [1, 2]:                       
+#         return 1
+#     else:
+#         return Fibonacci(n-1) + Fibonacci(n-2)
+# def NegaFibonacci(n):
+#     if n == 1:                       
+#         return 1
+#     elif n == 2:                       
+#         return -1
+#     else:
+#         num1, num2 = 1, -14
+#         for i in range(2, n):
+#             num1, num2 = num2, num1 - num2
+#         return num2
+# list = [0]
+# userNumber = int(input('Enter any number: '))
+# for e in range(1, userNumber + 1):
+#     list.append(Fibonacci(e))
+#     list.insert(0, NegaFibonacci(e))
+# print(list)
